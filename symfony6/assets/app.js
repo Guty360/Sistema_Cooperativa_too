7e021fc0f8a6b45ee018d19f9bf5485428c6c9e7
Guty360 <jmge5833@gmail.com>
2022-10-17 15:06:38 -0600

    Base agregar las vistas
    Cualquier error que tengan avisen

---
+++ symfony6/assets/app.js
@@ -0,0 +1,21 @@
+import './styles/app.css';
+import './bootstrap';
+import React from 'react';
+import ReactDOM from'react-dom';
+
+
+// console.log('ARTATE MIERDA GOCHEZ');
+
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>QUE PEDO, COMO LES VA</h1>
+            <p>Vamos a valer burguer</p>
+            <p>Att. Fox</p>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
