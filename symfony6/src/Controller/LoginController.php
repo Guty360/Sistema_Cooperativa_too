7b90dfbebfa2a292f18255d66a7ab1981150d48b
Guty360 <jmge5833@gmail.com>
2022-10-26 15:40:18 -0600

    Update back

--- symfony6/src/Controller/LoginController.php
+++ symfony6/src/Controller/LoginController.php
@@ -17,19 +17,4 @@ class LoginController extends AbstractController
         ]);
     }
 
-    public function datosLogin($correo, $contraseña)
-    {
-        $correo = $_POST['corr'];
-        $contraseña = $_POST['contra'];
-       
-        if(isset($_POST["correo"]) && $_POST["contraseña"]){
-            if($_POST['$correo']){
-
-                echo "correo recibido: " .$_POST['$correo'];
-        }else{
-            echo "correo no recibido";
-            }
-        }
-    }
-
 }
