3c80cf0644aee940895a861b3a0931d7b95a7793
lr18062 <lr18062@ues.edu.sv>
2022-10-28 16:41:30 -0600

    update personaController

--- symfony6/src/Controller/PersonaController.php
+++ symfony6/src/Controller/PersonaController.php
@@ -14,6 +14,12 @@ use SebastianBergmann\Environment\Console;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autolaod.php';
 
 class PersonaController extends AbstractController
 {
@@ -68,6 +74,35 @@ class PersonaController extends AbstractController
                 'encontrado' => true,
                 'data' => $personaAsArray
             ]);
+            //Create an instance; passing `true` enables exceptions
+            /*$mail = new PHPMailer(true);
+            try {
+                //Server settings
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                $mail->isSMTP();              
+                $mail->Host       = 'smtp-relay.gmail.com';
+                $mail->SMTPAuth   = true;
+                $mail->Username   = 'or030500@gmail.com';
+                $mail->Password   = 'qltrdggzrspiymxc';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port       = 587;
+                //Recipients
+                $mail->setFrom('or030500@gmail.com', 'Te envio un correo');
+                $mail->addAddress('sicosocial03@gmail.com', 'Recibo correo');
+                $mail->addCC('sicosocial03@gmail.com');
+                
+                //Content
+                $mail->isHTML(true);
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                echo 'Message has been sent';
+
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }*/
             return $response;
         }
         
